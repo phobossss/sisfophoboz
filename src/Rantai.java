1a4fb8f22177e089001ba84abed328b413bfd39e
ahmadulil <ahmadulil03@gmail.com>
2014-10-13 16:49:27 +0700

    Create Rantai.java

---
+++ src/Rantai.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+/**
+ *
+ * @author Compaq
+ */
+public class Rantai extends SparePart implements Interface {
+
+    public Rantai() {
+    }
+    
+
+    @Override
+    public void fungsi(String fngs) {
+        System.out.println("");
+    }
+
+    @Override
+    public void tambah(String tmbh) {
+        System.out.println("");
+    }
+
+    @Override
+    public void kurang(String krng) {
+        System.out.println("");
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+  
+    
+}
