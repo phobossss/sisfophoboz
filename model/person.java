38df431d809435aef5d925e333b1ec7d27d82593
ahmadulil <ahmadulil03@gmail.com>
2014-12-12 23:16:50 +0700

    Model dan View

---
+++ model/person.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Ulil
+ */
+public class person extends orang {
+
+    private String id_party;
+    private String id_person;
+
+    public person(String id_party, String name, String gender, String birthday, String deskripction, String street, int no) {
+        super(name, gender, birthday, deskripction, street, no);
+        this.id_party = id_party;
+        this.id_person = id_party+1;
+    }
+
+   
+
+    
+    public String getId_party() {
+        return id_party;
+    }
+
+    public void setId_party(String id_party) {
+        this.id_party = id_party;
+    }
+
+    
+
+    public String getId_person() {
+        return id_person;
+    }
+
+    public void setId_person(String id_person) {
+        this.id_person = id_person;
+    }
+
+}
---
+++ model/tools.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author Ulil
+ */
+public class tools {
+    
+    private String id_tool;
+    
+    private String deskripsi;
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getId_tool() {
+        return id_tool;
+    }
+
+    public void setId_tool(String id_tool) {
+        this.id_tool = id_tool;
+    }
+
+}
